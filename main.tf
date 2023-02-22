f1aa3e0c4300579527a641e75d1b3717ffec94b4
OranPoney <34721225+OranPoney@users.noreply.github.com>
2023-02-22 11:07:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "eks" {
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-version = "latest"
+version = "2.4"
 
   cluster_name                          = local.kubernetes_cluster_name
   cluster_version                       = var.kubernetes_cluster_version
