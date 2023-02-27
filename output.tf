f0868ecf11c35bb2a7d61d021eb046756443e14b
OranPoney <34721225+OranPoney@users.noreply.github.com>
2023-02-27 12:03:53 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,8 @@ output "kubernetes_ca_cert" {
 output "kubernetes_endpoint" {
   value = module.eks.cluster_endpoint
 }
+  
+output "cluster_id" {
+  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
+  value       = module.eks.cluster_id
+}
