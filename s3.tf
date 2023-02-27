1500f660f45959b2d4c1801c6f1832de07683350
OranPoney <34721225+OranPoney@users.noreply.github.com>
2023-02-27 10:52:48 +0200

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,24 @@
+module "s3_backup_bucket" {
+  count = var.kubernetes_backup_bucket_create ? 1 : 0
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+version = "1.20.0"
+
+  bucket = local.kubernetes_backup_bucket_name
+  acl    = "private"
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  tags = var.tags
+}
