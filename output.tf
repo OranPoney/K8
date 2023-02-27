acdfd113473b9ba2208ca826c31843d030af6e04
OranPoney <34721225+OranPoney@users.noreply.github.com>
2023-02-27 14:10:15 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,4 @@
-output "evolven_collection_token" {
-  value = data.kubernetes_secret.evolven_collection.data["token"]
-}
+
 
 output "kubernetes_ca_cert" {
   value = base64decode(module.eks.cluster_certificate_authority_data)
