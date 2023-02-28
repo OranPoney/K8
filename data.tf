38128793403537d59f39b4797eac51c995ad8076
OranPoney <34721225+OranPoney@users.noreply.github.com>
2023-02-28 20:26:13 +0200

    Update data.tf

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_arn
+  name = module.eks.name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
